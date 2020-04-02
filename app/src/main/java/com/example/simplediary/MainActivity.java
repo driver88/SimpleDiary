d08891c7ea4b603742e123a6bc42f6d9f66b6c5c
driver88 <50093035+driver88@users.noreply.github.com>
2020-04-02 12:46:33 +0900

    test code 추가

--- app/src/main/java/com/example/simplediary/MainActivity.java
+++ app/src/main/java/com/example/simplediary/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.simplediary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements OnTabItemSelectedListener {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.tab1:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                                return true;
+                            case R.id.tab2:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+                                return true;
+                            case R.id.tab3:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onTabSelected(int position){
+        switch (position){
+            case 0:
+                bottomNavigationView.setSelectedItemId(R.id.tab1);
+                break;
+            case 1:
+                bottomNavigationView.setSelectedItemId(R.id.tab2);
+                break;
+            case 2:
+                bottomNavigationView.setSelectedItemId(R.id.tab3);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/simplediary/OnTabItemSelectedListener.java
@@ -0,0 +1,5 @@
+package com.example.simplediary;
+
+public interface OnTabItemSelectedListener {
+    void onTabSelected(int position);
+}
